da0789adf322d6ce693107acd1b3afa50cf4ebea
Sandy Cruz <sandyleahcruz@icloud.com>
2021-09-04 10:48:21 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const index = () => {
+  return 'hi';
+};
+
+export default index;
---
+++ scripts/ci/eslint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "About to perform eslint run"
+
+yarn eslint
+
+echo "ESLint was a success"
